fc3fa00f28e89c41af67b746ea71ed30df117c48
Bhavin Gandhi <bhavin7392@gmail.com>
2022-03-02 22:47:00 +0530

    config: using 16 teeth pulley on X axis
    Signed-off-by: Bhavin Gandhi <bhavin7392@gmail.com>

--- marlin-config/Configuration.h
+++ marlin-config/Configuration.h
@@ -741,7 +741,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 2560, 94 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 80, 2560, 94 }
 
 /**
  * Default Max Feed Rate (mm/s)
