9275e8b972510c96eae202e7ea546c12a22d52df
Bhavin Gandhi <bhavin7392@gmail.com>
2022-03-05 22:24:45 +0530

    config: set the CRC sheet + MDF bed size
    Signed-off-by: Bhavin Gandhi <bhavin7392@gmail.com>

--- marlin-config/Configuration.h
+++ marlin-config/Configuration.h
@@ -1122,12 +1122,12 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 250
-#define Y_BED_SIZE 200
+#define X_BED_SIZE 270
+#define Y_BED_SIZE 270
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -10
-#define Y_MIN_POS -50
+#define X_MIN_POS 0
+#define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
