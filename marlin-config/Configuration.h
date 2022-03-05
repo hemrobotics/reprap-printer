359bcf91f99dbe1443561b95dfc8af28a6f83854
Bhavin Gandhi <bhavin7392@gmail.com>
2022-03-05 22:21:21 +0530

    config: X axis motor is on the left now
    Signed-off-by: Bhavin Gandhi <bhavin7392@gmail.com>

--- marlin-config/Configuration.h
+++ marlin-config/Configuration.h
@@ -620,10 +620,10 @@
 // Specify here all the endstop connectors that are connected to any endstop or probe.
 // Almost all printers will be using one per axis. Probes will use one or more of the
 // extra connectors. Leave undefined any used for non-endstop and non-probe purposes.
-//#define USE_XMIN_PLUG
+#define USE_XMIN_PLUG
 #define USE_YMIN_PLUG
 #define USE_ZMIN_PLUG
-#define USE_XMAX_PLUG
+//#define USE_XMAX_PLUG
 //#define USE_YMAX_PLUG
 //#define USE_ZMAX_PLUG
 
@@ -1115,7 +1115,7 @@
 
 // Direction of endstops when homing; 1=MAX, -1=MIN
 // :[-1,1]
-#define X_HOME_DIR 1
+#define X_HOME_DIR -1
 #define Y_HOME_DIR -1
 #define Z_HOME_DIR -1
 
