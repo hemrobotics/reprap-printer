961661f4649dea57417db5f39550352681ae892c
Bhavin Gandhi <bhavin7392@gmail.com>
2022-03-05 23:00:17 +0530

    config: enable skew compensation
    Signed-off-by: Bhavin Gandhi <bhavin7392@gmail.com>

--- marlin-config/Configuration.h
+++ marlin-config/Configuration.h
@@ -1435,17 +1435,17 @@
  *    +-------------->X     +-------------->X     +-------------->Y
  *     XY_SKEW_FACTOR        XZ_SKEW_FACTOR        YZ_SKEW_FACTOR
  */
-//#define SKEW_CORRECTION
+#define SKEW_CORRECTION
 
 #if ENABLED(SKEW_CORRECTION)
   // Input all length measurements here:
-  #define XY_DIAG_AC 282.8427124746
-  #define XY_DIAG_BD 282.8427124746
-  #define XY_SIDE_AD 200
+  #define XY_DIAG_AC 140.1
+  #define XY_DIAG_BD 142.1
+  #define XY_SIDE_AD 99.98
 
   // Or, set the default skew factors directly here
   // to override the above measurements:
-  #define XY_SKEW_FACTOR 0.0
+  //#define XY_SKEW_FACTOR 0.0
 
   //#define SKEW_CORRECTION_FOR_Z
   #if ENABLED(SKEW_CORRECTION_FOR_Z)
@@ -1459,7 +1459,7 @@
   #endif
 
   // Enable this option for M852 to set skew at runtime
-  //#define SKEW_CORRECTION_GCODE
+  #define SKEW_CORRECTION_GCODE
 #endif
 
 //=============================================================================
